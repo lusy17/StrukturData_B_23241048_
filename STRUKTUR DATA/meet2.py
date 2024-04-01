8bf822b6d9f9631f03dfccec7e1da1ae2e21adf2
Luxy <146051503+lusy17@users.noreply.github.com>
2024-04-01 11:09:11 +0700

    1/april/2024.LY

---
+++ STRUKTUR DATA/meet2.py
@@ -0,0 +1,21 @@
+# belajar Variabel 
+# assigmet
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =" , x)
+print ("Nilai UAS saya =" , y)
+print ("Nilai UAS saya =" , uang)
+
+# penamaan variabel 
+nilai_a = 9
+kotak10 = 10
+
+# input variabel kedua
+x = 30
+print ("Nilai UAS saya =" , x)
+x = 300
+print ("Nilai UAS saya =" , x)
+
