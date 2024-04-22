cf76365ccf7ac7eb4c3eb03fc9f84e1993b3a424
Luxy <146051503+lusy17@users.noreply.github.com>
2024-04-22 12:02:20 +0800

    22/april/2024.LY

---
+++ Lusiyanti11.py
@@ -0,0 +1,32 @@
+# contoh tipe data (integer): angka satuan
+bil_integer = 1009
+
+
+print (type(bil_integer))
+print ("type bilangan dari bil_integer adalah: ", type(bil_integer))
+
+# contoh tipe data (float): bilangan desimal
+
+bil_float = 2.5
+
+print ("type bilangan dari bil_float adalah: ", type(bil_float))
+
+
+# contoh tipe data (string) kumpulan karakter
+
+
+bil_string = "putu"
+
+
+print ("type bilangan dari bil_string adalah: ", type(bil_string))
+
+
+#contoh tipe data (bolean) biner, 0,1, True False
+
+
+
+bil_bol True
+
+
+
+print ("type bilangan dari bil_bol adalah: ", type(bil_bol))
