d4ee8b374c8af7732454650a692b2fbcea907647
Luxy <146051503+lusy17@users.noreply.github.com>
2024-06-10 12:25:53 +0800

    10/6/24.Ly

---
+++ Meet10.py
@@ -0,0 +1,39 @@
+# Belajar perbandingan lanjutan ELIF
+
+#nama = input ("Masukan nama :")
+
+#if nama == "LY":
+#    print("mahasiswi pti")
+#elif nama == "ecy":
+    #print("mahasiswi pti")
+#elif nama == "cy":
+#    print("mahasiswi pti")
+#elif nama == "lie":
+#    print("mahasiswi pti")
+#elif nama == "lyy":
+#    print("mahasiswi pti")
+#elif nama == "clyn":
+ #   print("mahasiswi pti")
+#elif nama == "Ly":
+ #   print("mahasiswi pti")
+#else:
+  #  print("bukan mahasiswi pti")
+  #  print("program selesai")
+  
+  # Aplikasi Penjumlahan Sederhana
+
+# Meminta input dari pengguna
+num1 = input("Masukkan angka pertama: ")
+num2 = input("Masukkan angka kedua: ")
+
+# Mengubah input menjadi bilangan bulat
+num1 = int(num1)
+num2 = int(num2)
+
+# Menjumlahkan dua bilangan
+result = num1 + num2
+
+# Menampilkan hasil penjumlahan
+print("Hasil penjumlahan:", result)
+
+       
