7d547304e994459abe91d08317d1c19d584d7443
Luxy <146051503+lusy17@users.noreply.github.com>
2024-07-01 11:12:30 +0800

    PR.py

---
+++ PR_CARA 2_Ly.py
@@ -0,0 +1,8 @@
+
+MasukanUser = int(input("Masukan bilangan 5 sampai 9 dan lebih dari 12 sampai 30 : "))
+
+# ------5++++++9------12+++++30
+if 5 <= MasukanUser <= 9 or MasukanUser >= 12 and MasukanUser <= 30:
+    print("True")
+else:
+    print("False")
