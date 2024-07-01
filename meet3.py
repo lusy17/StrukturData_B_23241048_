124916ec110d8bf4708dfadbcbf717130c661c9c
Luxy <146051503+lusy17@users.noreply.github.com>
2024-07-01 10:07:28 +0800

    1/7/2024.UAS.LY

---
+++ meet3.py
@@ -0,0 +1,13 @@
+angka = 15
+
+# Percabangan
+if angka > 15:
+    print("Angka lebih besar dari 15")
+elif angka < 15:
+    print("Angka lebih kecil dari 15")
+else:
+    print("Angka sama dengan 15")
+
+# Perulangan
+for i in range(12):
+    print("Ini adalah perulangan ke-", i+1)
